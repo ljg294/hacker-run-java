c6bef674a7b90ab9f904f18ff37e01a3f0d6eee5
ljg294 <jay@Jaygues-MacBook-Pro.local>
2023-08-29 13:03:48 +0900

    Problem 4 solution

---
+++ src/main/java/Problem4.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+//Java Stdin and Stdout II
+public class Problem4 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int integerValue = scanner.nextInt();
+
+        double doubleValue = scanner.nextDouble();
+        scanner.nextLine();
+        String stringValue = scanner.nextLine();
+
+        System.out.println("String: " + stringValue);
+        System.out.println("Double: " + doubleValue);
+        System.out.println("Int: " + integerValue);
+
+        scanner.close();
+    }
+
+}
