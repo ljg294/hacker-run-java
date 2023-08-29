f3659c8b12b106350b621b5e5a67b954f4f76fdd
ljg294 <jay@Jaygues-MacBook-Pro.local>
2023-08-29 13:43:26 +0900

    Problem 6 solution

---
+++ src/main/java/Problem6.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+//Java Loops I
+public class Problem6 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        for(int i=1; i<=10; i++)
+        {
+            System.out.println(n +" x " +i +" = " +n*i);
+        }
+    }
+
+}
